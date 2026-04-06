fa07dc66dc4a7a644abda481aa51386b8e3ae185
Bao Tran <151926857+tranbao01@users.noreply.github.com>
2026-04-06 13:48:14 +1000

    Add axios routes for ISBN, author, title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,39 +36,76 @@ public_users.get('/books', function (req, res) {
   res.send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+// Get book details by ISBN (direct data source for async/await route)
+public_users.get('/books/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
   if (book) {
-    res.send(JSON.stringify(book,null,4));
-  } else {
-    res.status(404).json({message: "Book not found"});
+    return res.send(JSON.stringify(book, null, 4));
+  }
+  return res.status(404).json({ message: "Book not found" });
+});
+
+// Get book details by author (direct data source for async/await route)
+public_users.get('/books/author/:author', function (req, res) {
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(
+    (book) => book.author.toLocaleLowerCase() === author.toLocaleLowerCase()
+  );
+  if (booksByAuthor.length > 0) {
+    return res.send(JSON.stringify(booksByAuthor, null, 4));
+  }
+  return res.status(404).json({ message: "No books found by this author" });
+});
+
+// Get book details by title (direct data source for async/await route)
+public_users.get('/books/title/:title', function (req, res) {
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(
+    (book) => book.title.toLocaleLowerCase() === title.toLocaleLowerCase()
+  );
+  if (booksByTitle.length > 0) {
+    return res.send(JSON.stringify(booksByTitle, null, 4));
+  }
+  return res.status(404).json({ message: "No books found with this title" });
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', async function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  try {
+    const baseUrl = `${req.protocol}://${req.get('host')}`;
+    const response = await axios.get(`${baseUrl}/books/isbn/${isbn}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(404).json({ message: "Book not found" });
   }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   //Write your code here
   const author = req.params.author;
-  const booksByAuthor = Object.values(books).filter(book => book.author.toLocaleLowerCase() === author.toLocaleLowerCase());
-  if (booksByAuthor.length > 0) {
-    res.send(JSON.stringify(booksByAuthor,null,4));
-  } else {
-    res.status(404).json({message: "No books found by this author"});
+  try {
+    const baseUrl = `${req.protocol}://${req.get('host')}`;
+    const response = await axios.get(`${baseUrl}/books/author/${encodeURIComponent(author)}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(404).json({ message: "No books found by this author" });
   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
   const title = req.params.title;
-  const booksByTitle = Object.values(books).filter(book => book.title.toLocaleLowerCase() === title.toLocaleLowerCase());
-  if (booksByTitle.length > 0) {
-    res.send(JSON.stringify(booksByTitle,null,4));
-  } else {
-    res.status(404).json({message: "No books found with this title"});
+  try {
+    const baseUrl = `${req.protocol}://${req.get('host')}`;
+    const response = await axios.get(`${baseUrl}/books/title/${encodeURIComponent(title)}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(404).json({ message: "No books found with this title" });
   }
 });
 
